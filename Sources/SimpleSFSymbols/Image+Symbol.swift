36f5af301132860b752647041a209245d954ef63
Corey Walo <cjwalo@gmail.com>
2025-04-23 18:47:52 -0700

    Update extensions

--- Sources/SimpleSFSymbols/Image+Symbol.swift
+++ Sources/SimpleSFSymbols/Image+Symbol.swift
@@ -8,8 +8,20 @@
 import SwiftUI
 
 extension Image {
-    @available(iOS 13, macOS 11.0, tvOS 13, visionOS 1, watchOS 6, *)
+    @available(iOS 13.0, macOS 11.0, tvOS 13.0, visionOS 1.0, watchOS 6.0, *)
     public init(_ symbol: Symbol) {
         self.init(systemName: symbol.systemName)
+    }
+    
+    @available(iOS 16.0, macOS 13.0, tvOS 16.0, visionOS 1, watchOS 9.0, *)
+    public init(_ symbol: Symbol, variableValue: Double?) {
+        self.init(systemName: symbol.systemName, variableValue: variableValue)
+    }
+}
+
+@available(iOS 14.0, macOS 11.0, tvOS 14.0, visionOS 1.0, watchOS 7.0, *)
+extension Label where Icon == Image, Title == Text {
+    public init(_ titleKey: LocalizedStringKey, symbol: Symbol) {
+        self.init(titleKey, systemImage: symbol.systemName)
     }
 }
